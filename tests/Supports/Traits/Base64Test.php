6280cbdebf563d91042e77ef99ad029e8165f54e
Alireza Mohammadi <mohammadialireza238@gmail.com>
2023-07-18 20:25:46 +0200

    Adds tests for traits.

---
+++ tests/Supports/Traits/Base64Test.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AlirezaMoh\JwtShield\Tests\Supports\Traits;
+
+use AlirezaMoh\JwtShield\Supports\Traits\Base64;
+use PHPUnit\Framework\TestCase;
+
+class Base64Test extends TestCase
+{
+    use Base64;
+
+    private array $encodeData;
+    private string $decodedData;
+
+    protected function setUp(): void
+    {
+        $this->encodeData = [
+            "userId" => 54544542,
+            "username" => "test",
+        ];
+
+        $this->decodedData = "eyJ1c2VySWQiOjU0NTQ0NTQyLCJ1c2VybmFtZSI6InRlc3QifQ";
+    }
+
+    /**
+     * @test
+     */
+    public function should_encode_base64_string()
+    {
+        $actual = $this->encodeBase64(json_encode($this->encodeData));
+
+        $this->assertEquals($this->decodedData, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function should_decode_base64_string()
+    {
+        $actual = $this->decodeBase64($this->decodedData);
+
+        $this->assertEquals($this->encodeData, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function should_return_url_safe_base64_string()
+    {
+        $expectedData = "VGhpcyBpcyBhIHRlc3QgZGF0YSB0byBlbmNvZGU9";
+        $actual = $this->getUrlSafe("VGhpcyBpcyBhIHRlc3QgZGF0YSB0byBlbmNvZGU9=");
+
+        $this->assertEquals($expectedData, $actual);
+    }
+}
